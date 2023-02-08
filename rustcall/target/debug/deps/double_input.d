35d7b950c350172278a71535c4b5aacb96a5ec00
rtafds <n.rtafds@gmail.com>
2023-02-08 14:49:09 +0900

    add: rustcall

---
+++ rustcall/target/debug/deps/double_input.d
@@ -0,0 +1,5 @@
+/Users/smamitsu/Github/Julia_study/rustcall/target/debug/deps/libdouble_input.dylib: src/lib.rs
+
+/Users/smamitsu/Github/Julia_study/rustcall/target/debug/deps/double_input.d: src/lib.rs
+
+src/lib.rs:
---
+++ rustcall/target/debug/libdouble_input.d
@@ -0,0 +1,1 @@
+/Users/smamitsu/Github/Julia_study/rustcall/target/debug/libdouble_input.dylib: /Users/smamitsu/Github/Julia_study/rustcall/src/lib.rs
