c628659d2b290de54daec52a3b9701a390d904ac
rtafds <n.rtafds@gmail.com>
2023-02-10 12:43:58 +0900

    update: pycall

---
+++ pythoncalljulia/PyJulia/test.jl
@@ -0,0 +1,3 @@
+function twice_array(x::Array{T}) where T <: Number
+    return 2x
+end
---
+++ pythoncalljulia/PythonCall/test.jl
@@ -0,0 +1,11 @@
+using DataFrames
+
+# train_test_splitの関数
+function partitionTrainTest(data, at = 0.7)
+    n = nrow(data)
+    idx = shuffle(1:n)
+    train_idx = view(idx, 1:floor(Int, at*n))
+    test_idx = view(idx, (floor(Int, at*n)+1):n)
+    data[train_idx,:], data[test_idx,:]
+end
+
